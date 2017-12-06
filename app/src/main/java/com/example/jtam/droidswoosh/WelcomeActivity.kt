490ffd1ec8f1f4be16d3a7ceb3653b9aa6c359e9
dev-tam <tamjid.mdh@gmail.com>
2017-12-07 03:08:18 +0600

    Activity handling

--- app/src/main/java/com/example/jtam/droidswoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/jtam/droidswoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.jtam.droidswoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    }
+
+    fun getStartedClicked(view: View){
+        val startedActivity = Intent(this, LeagueActivity::class.java)
+        startActivity(startedActivity)
     }
 }
