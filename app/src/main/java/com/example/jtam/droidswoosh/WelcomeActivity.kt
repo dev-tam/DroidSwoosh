c8d9c6758f3f841c4cd7405f598cbf138a5e6fdd
dev-tam <tamjid.mdh@gmail.com>
2017-12-07 03:33:32 +0600

    Fixed Welcomes Intent

--- app/src/main/java/com/example/jtam/droidswoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/jtam/droidswoosh/WelcomeActivity.kt
@@ -1,19 +1,26 @@
 package com.example.jtam.droidswoosh
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+//import android.view.View
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 
-    fun getStartedClicked(view: View){
-        val startedActivity = Intent(this, LeagueActivity::class.java)
-        startActivity(startedActivity)
-    }
+
+//    fun getStartedClicked(view: View){
+//        val startedActivity = Intent(this, LeagueActivity::class.java)
+//        startActivity(startedActivity)
+//    }
 }
